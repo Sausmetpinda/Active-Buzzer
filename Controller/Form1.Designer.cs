2a322543ef172cda58004ad3284660b62951fc17
BuildTools <unconfigured@null.spigotmc.org>
2021-10-30 19:34:42 +0200

    Add project files.

---
+++ Controller/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Toggle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // port
+            // 
+            this.port.PortName = "COM3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(169, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 76);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Beep";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "On/Off box";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Morse code translator:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 211);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(328, 250);
+            this.MinimumSize = new System.Drawing.Size(328, 191);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Controller/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            port.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            write("toggle");
+        }
+        void write(string data)
+        {
+            port.Write(data);
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            write("toggle");
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            write("toggle");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            write("beep");
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        { 
+            if (e.KeyCode == Keys.Enter)
+            {
+                string endString = "";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                foreach(char l in textBox1.Text)
+                {
+                    endString += " / ";
+                    endString += charToMorse(l);
+                }
+                write(endString);
+            }
+        }
+        string charToMorse(char c)
+        {
+            switch (c)
+            {
+                case 'a':
+                    return ".-";
+                case 'b':
+                    return "-...";
+                case 'c':
+                    return "-.-.";
+                case 'd':
+                    return "-..";
+                case 'e':
+                    return ".";
+                case 'f':
+                    return "..-.";
+                case 'g':
+                    return "--.";
+                case 'h':
+                    return "....";
+                case 'i':
+                    return "..";
+                case 'j':
+                    return ".---";
+                case 'k':
+                    return "-.-";
+                case 'l':
+                    return ".-..";
+                case 'm':
+                    return "--";
+                case 'n':
+                    return "-.";
+                case 'o':
+                    return "---";
+                case 'p':
+                    return ".--.";
+                case 'q':
+                    return "--.-";
+                case 'r':
+                    return ".-.";
+                case 's':
+                    return "...";
+                case 't':
+                    return "-";
+                case 'u':
+                    return "..-";
+                case 'v':
+                    return "...-";
+                case 'w':
+                    return ".--";
+                case 'x':
+                    return "-..-";
+                case 'y':
+                    return "-.--";
+                case 'z':
+                    return "--..";
+                default:
+                    return null;
+            }
+        }
+    }
+}
