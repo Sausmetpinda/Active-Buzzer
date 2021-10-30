6ffefb392d111b0a7cdb57ceec386dfaa258883f
BuildTools <unconfigured@null.spigotmc.org>
2021-10-30 19:37:30 +0200

    a

--- Controller/Form1.Designer.cs
+++ Controller/Form1.Designer.cs
@@ -117,7 +117,6 @@ namespace Controller
             this.MaximumSize = new System.Drawing.Size(328, 250);
             this.MinimumSize = new System.Drawing.Size(328, 191);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
